729947617ec43f17c78edafedd4aeb7e6bfa88f2
Cao Phuoc Thanh <caophuocthanh@gmail.com>
2021-03-25 17:35:14 +0700

    first

---
+++ server-dav/index.js
@@ -0,0 +1,18 @@
+const webdav = require('webdav-server').v2;
+
+const server = new webdav.WebDAVServer({
+    port: 1900
+});
+
+server.afterRequest((arg, next) => {
+    // Display the method, the URI, the returned status code and the returned message
+    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+    // If available, display the body of the response
+    console.log(arg.responseBody);
+    next();
+});
+
+
+server.setFileSystem('/local', new webdav.PhysicalFileSystem('./local'), (success) => {
+    server.start(() => console.log('READY'));
+})
